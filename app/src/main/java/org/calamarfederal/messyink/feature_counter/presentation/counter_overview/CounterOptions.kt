17333c08e6cf889ea65d4a730a0efad60027c1f1
squiddy <squiddy@squiddy.org>
2023-04-28 11:14:51 -0500

    refactorer counter options to new file

---
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOptions.kt
@@ -0,0 +1,57 @@
+package org.calamarfederal.messyink.feature_counter.presentation.counter_overview
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ClearAll
+import androidx.compose.material.icons.filled.DeleteForever
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Popup
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun CounterOptionsPopup(
+    visible: Boolean,
+    onDismiss: () -> Unit,
+    onDelete: () -> Unit,
+    onClear: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AnimatedVisibility(visible = visible, modifier = modifier) {
+        Popup(
+            onDismissRequest = onDismiss,
+            alignment = Alignment.BottomCenter,
+        ) {
+            ElevatedCard {
+                Row {
+                    InputChip(
+                        selected = false,
+                        onClick = onClear,
+                        border = null,
+                        label = { Text("Clear") },
+                        leadingIcon = { Icon(Filled.ClearAll, "clear all") },
+                    )
+//                    Divider()
+                    InputChip(
+                        selected = false,
+                        onClick = onDelete,
+                        border = null,
+                        label = { Text("Delete") },
+                        leadingIcon = {
+                            Icon(Filled.DeleteForever, "delete and its contents")
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOverviewScreen.kt
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOverviewScreen.kt
@@ -1,53 +1,31 @@
 package org.calamarfederal.messyink.feature_counter.presentation.counter_overview
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ClearAll
-import androidx.compose.material.icons.filled.DeleteForever
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.InputChip
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.LocalAbsoluteTonalElevation
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SelectableChipBorder
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
-import org.calamarfederal.messyink.common.compose.toStringAllowShorten
 import org.calamarfederal.messyink.feature_counter.presentation.state.UiCounter
 import org.calamarfederal.messyink.feature_counter.presentation.state.previewUiCounters
 import org.calamarfederal.messyink.ui.theme.MessyInkTheme
-import org.calamarfederal.messyink.ui.theme.TonalElevation
 
 @Preview
 @Composable
@@ -125,7 +103,7 @@ private fun CounterOverviewLayout(
                     )
                 )
 
-                CounterOptions(
+                CounterOptionsPopup(
                     visible = showOptions,
                     onDismiss = { showOptions = false },
                     onDelete = { onDeleteCounter(counter) },
@@ -135,44 +113,4 @@ private fun CounterOverviewLayout(
             }
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun CounterOptions(
-    visible: Boolean,
-    onDismiss: () -> Unit,
-    onDelete: () -> Unit,
-    onClear: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    AnimatedVisibility(visible = visible, modifier = modifier) {
-        Popup(
-            onDismissRequest = onDismiss,
-            alignment = Alignment.BottomCenter,
-        ) {
-            ElevatedCard {
-                Row {
-                    InputChip(
-                        selected = false,
-                        onClick = onClear,
-                        border = null,
-                        label = { Text("Clear") },
-                        leadingIcon = { Icon(Icons.Filled.ClearAll, "clear all") },
-                    )
-//                    Divider()
-                    InputChip(
-                        selected = false,
-                        onClick = onDelete,
-                        border = null,
-                        label = { Text("Delete") },
-                        leadingIcon = {
-                            Icon(Icons.Filled.DeleteForever, "delete and its contents")
-                        }
-                    )
-                }
-            }
-        }
-    }
-
 }
