bfc94e851cf4fb52a0c10e48b36e8077cadb0552
squiddy <squiddy@squiddy.org>
2023-04-27 19:03:20 -0500

    add label for custom tick entry

--- app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/game_counter/CustomTickDialog.kt
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/game_counter/CustomTickDialog.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,6 +19,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +28,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import org.calamarfederal.messyink.common.compose.Placeholder
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun TickDialoguePreview() {
     CustomTickEntryDialog(visible = true, onDismiss = {}, onAddTick = {})
@@ -41,6 +44,7 @@ internal fun CustomTickEntryDialog(
     AnimatedVisibility(visible = visible) {
         Dialog(onDismissRequest = onDismiss) {
             Column {
+                Text("Increment by:")
                 CustomTickEntry(onAddTick = { onAddTick(it); onDismiss() }, modifier = modifier)
             }
         }
