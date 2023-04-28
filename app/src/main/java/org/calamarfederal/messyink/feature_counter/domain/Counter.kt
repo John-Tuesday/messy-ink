b58d6b42ad4fa39108f333a3de52d1efabdf4788
squiddy <squiddy@squiddy.org>
2023-04-28 00:08:11 -0500

    document Counter and Tick

--- app/src/main/java/org/calamarfederal/messyink/feature_counter/domain/Counter.kt
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/domain/Counter.kt
@@ -2,6 +2,14 @@ package org.calamarfederal.messyink.feature_counter.domain
 
 import kotlinx.datetime.Instant
 
+/**
+ * Holds the metadata for a collection of ticks, i.e. the parent / the owner
+ *
+ * @property[name] name given by the user; not unique
+ * @property[timeModified] time of last modification of this Counter its Tick
+ * @property[timeCreated] time the Counter was originally made
+ * @property[id] unique identifier; also used for [Tick.parentId]
+ */
 data class Counter(
     val name: String,
     val timeModified: Instant,
@@ -9,6 +17,15 @@ data class Counter(
     val id: Long,
 )
 
+/**
+ * Single event to be counted
+ *
+ * @property[amount] value to measured
+ * @property[timeCreated] time the [Tick] was created; for use with history and logs
+ * @property[timeForData] time to be used when analyzing; e.g. graphing
+ * @property[parentId] id of owning [Counter]
+ * @property[id] unique identity; (unique across all ticks)
+ */
 data class Tick(
     val amount: Double,
     val timeCreated: Instant,
