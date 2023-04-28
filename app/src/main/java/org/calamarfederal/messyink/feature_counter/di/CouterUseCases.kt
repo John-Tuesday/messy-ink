d3be139e490c66159582e69b106a2985149beb8c
squiddy <squiddy@squiddy.org>
2023-04-27 23:23:59 -0500

    remove obselete code

--- app/src/main/java/org/calamarfederal/messyink/feature_counter/di/CouterUseCases.kt
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/di/CouterUseCases.kt
@@ -1,38 +1,3 @@
 package org.calamarfederal.messyink.feature_counter.di
 
-//@Module
-//@InstallIn(ViewModelComponent::class)
-//object CouterUseCases {
-//    @Provides
-//    @ViewModelScoped
-//    fun provideGetCountersFlows(repo: CountersRepo): GetCountersFlow = GetCountersFlowImpl(repo)
-//
-//    @Provides
-//    @ViewModelScoped
-//    fun provideGetTicksOfFlows(repo: CountersRepo): GetTicksOfFlow = GetTicksOfFlowImpl(repo)
-//
-//    @Provides
-//    @ViewModelScoped
-//    fun provideCreateCounterFrom(repo: CountersRepo): CreateCounterFrom = CreateCounterFromImpl(repo)
-//
-//    @Provides
-//    @ViewModelScoped
-//    fun provideCreateTickFrom(repo: CountersRepo): CreateTickFrom = CreateTickFromImpl(repo)
-//
-//    /**
-//     * ## Calculation
-//     */
-//
-//    @Provides
-//    @ViewModelScoped
-//    fun provideGetTicksSumOfFlow(repo: CountersRepo): GetTicksSumOfFlow = GetTicksSumOfFlowImpl(repo)
-//
-//    @Provides
-//    @ViewModelScoped
-//    fun provideGetTicksAverageOfFlow(repo: CountersRepo): GetTicksAverageFlow = GetTicksAverageOfFlowImpl(repo)
-//
-//    @Provides
-//    @ViewModelScoped
-//    fun provideGetSumOfTicksByFlow(repo: CountersRepo): GetTicksSumByFlow = GetTicksSumByFlowImpl(repo)
-//
-//}
+
