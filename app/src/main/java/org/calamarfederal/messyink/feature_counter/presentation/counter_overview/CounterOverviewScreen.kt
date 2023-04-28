31f085592dac4c24bacf0073b78352c4b6db25e9
squiddy <squiddy@squiddy.org>
2023-04-28 11:12:47 -0500

    add divider to counter options

--- app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOverviewScreen.kt
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOverviewScreen.kt
@@ -7,10 +7,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.IntrinsicSize.Max
+import androidx.compose.foundation.layout.IntrinsicSize.Min
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -40,6 +45,7 @@ import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
@@ -151,7 +157,7 @@ private fun CounterOptions(
             onDismissRequest = onDismiss,
             alignment = Alignment.BottomCenter,
         ) {
-            ElevatedCard {
+            ElevatedCard(modifier = Modifier.height(Min)) {
                 Row {
                     InputChip(
                         selected = false,
@@ -160,7 +166,7 @@ private fun CounterOptions(
                         label = { Text("Clear") },
                         leadingIcon = { Icon(Icons.Filled.ClearAll, "clear all") },
                     )
-//                    Divider()
+                    Divider(modifier = Modifier.width(1.dp).fillMaxHeight().padding(vertical = 4.dp))
                     InputChip(
                         selected = false,
                         onClick = onDelete,
