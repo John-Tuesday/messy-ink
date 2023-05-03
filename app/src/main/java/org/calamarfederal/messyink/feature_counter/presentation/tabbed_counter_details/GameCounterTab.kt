4aac7b65bf8918ff96e738f5271cca54ab4c4f38
squiddy <squiddy@squiddy.org>
2023-05-03 00:37:56 -0500

    remove commented-out class and rename file to match its purpose

--- app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/tabbed_counter_details/GameCounterTab.kt
+++ app/src/main/java/org/calamarfederal/messyink/feature_counter/presentation/tabbed_counter_details/GameCounterTab.kt
@@ -6,16 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import org.calamarfederal.messyink.feature_counter.presentation.state.UiCounter
 
-///**
-// * Represent all the tabs of the TabbedCounterDetailsScreen
-// *
-// * @param[tab] used as a [Tab] in a [TabRow]
-// * @param[content] displayed when tab is selected
-// */
-//sealed class CounterTab (val tab: @Composable (Boolean) -> Unit, val content: @Composable () -> Unit) {
-//
-//}
-
 @Composable
 internal fun GameCounterTab(
     counter: UiCounter,
