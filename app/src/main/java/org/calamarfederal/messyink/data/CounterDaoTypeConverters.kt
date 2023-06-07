434528fb7a3e20da6cd6d4ddec0aee5be9104adc
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-06-06 21:35:32 -0500

    clarify timestamp is in milli not sec

--- app/src/main/java/org/calamarfederal/messyink/data/CounterDaoTypeConverters.kt
+++ app/src/main/java/org/calamarfederal/messyink/data/CounterDaoTypeConverters.kt
@@ -3,15 +3,24 @@ package org.calamarfederal.messyink.data
 import androidx.room.TypeConverter
 import kotlinx.datetime.Instant
 
+/**
+ * Convert [Instant] to a room compatible type
+ */
 class TimeTypeConverters {
 //    @TypeConverter
 //    fun fromInstantToTimeStamp(instant: Instant?): Long? = instant?.toEpochMilliseconds()
 //    @TypeConverter
 //    fun fromTimestampToInstant(stamp: Long?): Instant? = stamp?.let {Instant.fromEpochMilliseconds(it) }
 
+    /**
+     * Unix milliseconds -- not seconds
+     */
     @TypeConverter
     fun fromInstantToTimeStamp(instant: Instant): Long = instant.toEpochMilliseconds()
 
+    /**
+     * Unix milliseconds -- not seconds
+     */
     @TypeConverter
     fun fromTimestampToInstant(stamp: Long): Instant = Instant.fromEpochMilliseconds(stamp)
 }
