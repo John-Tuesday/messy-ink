f8afa2baf8ce0855a1fab0d7c35f8a3023247c75
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-06-18 00:56:46 -0500

    create base test hilt modules in android test

---
+++ app/src/androidTest/java/org/calamarfederal/messyink/di/MessyInkTestModule.kt
@@ -0,0 +1,23 @@
+package org.calamarfederal.messyink.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import org.calamarfederal.messyink.data.MessyInkDb
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [MessyInkTestModule::class]
+)
+class MessyInkTestModule {
+    @Provides
+    @Singleton
+    fun providesTestDbInMemory(app: Application): MessyInkDb = Room
+        .inMemoryDatabaseBuilder(app, MessyInkDb::class.java)
+        .build()
+}
