d432fd619f03fd4ebdf0a8c8b9c081a8134966a7
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-06-21 20:53:42 -0500

    fix inconsistent and incompatible depencies and clean up

---
+++ app/src/androidTest/java/org/calamarfederal/messyink/feature_counter/presentation/create_counter/CreateCounterScreenKtTest.kt
@@ -0,0 +1,17 @@
+package org.calamarfederal.messyink.feature_counter.presentation.create_counter
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class CreateCounterScreenKtTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun createCounterScreen() {
+    }
+}
