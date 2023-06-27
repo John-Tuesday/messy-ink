eaf67e40a00f816e18bb5b5c3fa2776f908b8bd4
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-06-27 14:45:00 -0500

    Rename dao type converters file. Add presentation Test Suite

---
+++ app/src/androidTest/java/org/calamarfederal/messyink/feature_counter/presentation/PresentationTestSuite.kt
@@ -0,0 +1,19 @@
+package org.calamarfederal.messyink.feature_counter.presentation
+
+import org.calamarfederal.messyink.feature_counter.presentation.counter_history.CounterHistoryScreenTest
+import org.calamarfederal.messyink.feature_counter.presentation.counter_overview.CounterOverviewScreenTest
+import org.calamarfederal.messyink.feature_counter.presentation.create_counter.CreateCounterScreenKtTest
+import org.calamarfederal.messyink.feature_counter.presentation.navigation.CounterFeatureNavigationTests
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+@RunWith(Suite::class)
+@SuiteClasses(
+    CounterHistoryScreenTest::class,
+    CounterOverviewScreenTest::class,
+    CreateCounterScreenKtTest::class,
+    CounterFeatureNavigationTests::class,
+)
+class PresentationTestSuite {
+}
