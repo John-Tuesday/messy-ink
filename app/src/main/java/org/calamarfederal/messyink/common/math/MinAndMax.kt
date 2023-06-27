19bcad401643358c79a19728642585e49f58cb0e
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-06-27 02:28:36 -0500

    change MinMax to non-data class to remove copy function

--- app/src/main/java/org/calamarfederal/messyink/common/math/MinAndMax.kt
+++ app/src/main/java/org/calamarfederal/messyink/common/math/MinAndMax.kt
@@ -1,18 +1,51 @@
 package org.calamarfederal.messyink.common.math
 
 /**
- * holds the lowest and highest value, but [min] < [max] is not enforced
+ * Minimum and Maximum values
  *
- * @property[min] minimum
- * @property[max] maximum
+ * primary constructor performs no checks. use [minMaxOf] instead to init [min] and [max]
  */
-data class MinMax<T : Comparable<T>>(
+class MinMax<T : Comparable<T>>(
+    /**
+     * Minimum value
+     *
+     * Convention should follow:
+     * `[min] <= [max]`
+     */
     val min: T,
+    /**
+     * Maximum value
+     *
+     * Convention should follow:
+     * `[min] <= [max]`
+     */
     val max: T,
 ) : ClosedRange<T> {
     override val start = min
     override val endInclusive = max
+
+    override fun equals(other: Any?): Boolean =
+        if (other !is MinMax<*>) false else other.min == min && other.max == max
+
+    override fun hashCode(): Int {
+        var result = min.hashCode()
+        result = 31 * result + max.hashCode()
+        return result
+    }
+
 }
+
+/**
+ * Properly create and initialize [MinMax]
+ */
+fun <T : Comparable<T>> minMaxOf(a: T, b: T): MinMax<T> =
+    MinMax(min = minOf(a, b), max = maxOf(a, b))
+
+/**
+ * [MinMax] which expands to include [other]
+ */
+infix fun <T : Comparable<T>> MinMax<T>.include(other: T): MinMax<T> =
+    MinMax(min = minOf(min, other), max = maxOf(max, other))
 
 /**
  * Gets the minimum and maximum in one sweep; Or null if empty
@@ -22,10 +55,7 @@ data class MinMax<T : Comparable<T>>(
 fun <T, R : Comparable<R>> Iterable<T>.minAndMaxOfOrNull(selector: (T) -> R): MinMax<R>? =
     fold<T, MinMax<R>?>(null) { acc, item ->
         val value = selector(item)
-        acc?.copy(
-            min = minOf(acc.min, value),
-            max = maxOf(acc.max, value),
-        ) ?: MinMax(value, value)
+        acc?.include(value) ?: MinMax(value, value)
     }
 
 /**
