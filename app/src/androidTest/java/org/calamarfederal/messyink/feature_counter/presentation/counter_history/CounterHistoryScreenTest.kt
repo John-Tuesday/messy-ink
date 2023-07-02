16f2c7c20f1b05b473db284c3804d1af9a0e17fd
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2023-07-02 11:07:47 -0500

    remove unnecessary hilt injection

--- app/src/androidTest/java/org/calamarfederal/messyink/feature_counter/presentation/counter_history/CounterHistoryScreenTest.kt
+++ app/src/androidTest/java/org/calamarfederal/messyink/feature_counter/presentation/counter_history/CounterHistoryScreenTest.kt
@@ -1,37 +1,84 @@
 package org.calamarfederal.messyink.feature_counter.presentation.counter_history
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import dagger.hilt.android.testing.BindValue
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import org.calamarfederal.messyink.MainActivity
-import org.calamarfederal.messyink.OnCreateHookImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.datetime.Instant
+import kotlinx.datetime.Instant.Companion
+import org.calamarfederal.messyink.feature_counter.data.generateCounters
+import org.calamarfederal.messyink.feature_counter.data.generateTicks
+import org.calamarfederal.messyink.feature_counter.data.toCounter
+import org.calamarfederal.messyink.feature_counter.data.toTick
+import org.calamarfederal.messyink.feature_counter.domain.use_case.toUI
+import org.calamarfederal.messyink.feature_counter.domain.use_case.toUi
+import org.calamarfederal.messyink.feature_counter.presentation.state.TimeDomain
+import org.calamarfederal.messyink.feature_counter.presentation.state.UiCounter
+import org.calamarfederal.messyink.feature_counter.presentation.state.UiTick
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.stream.Collectors.toList
 
-@HiltAndroidTest
 class CounterHistoryScreenTest {
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
+    @get:Rule
+    val composeRule = createComposeRule()
 
-    @get:Rule(order = 1)
-    val composeRule = createAndroidComposeRule<MainActivity>()
-
-    private val testContent = CounterHistoryTestContent()
-
-    @BindValue
-    val contentSetter: OnCreateHookImpl = testContent
+    lateinit var counterState: MutableStateFlow<UiCounter>
+    lateinit var ticksState: MutableStateFlow<List<UiTick>>
 
     @Before
     fun setUp() {
-        hiltRule.inject()
+        counterState = MutableStateFlow(generateCounters().take(1).first().toCounter().toUI())
+        ticksState = MutableStateFlow(
+            generateTicks(parentId = counterState.value.id).take(10).map { it.toTick().toUi() }
+                .toList()
+        )
+
+        composeRule.setContent {
+            val counter by counterState.collectAsState()
+            val ticks by ticksState.collectAsState()
+            val tickSum by remember {
+                derivedStateOf { ticks.sumOf { it.amount } }
+            }
+            val tickAverage by remember {
+                derivedStateOf { ticks.sumOf { it.amount } / ticks.size }
+            }
+            val graphRange by remember {
+                derivedStateOf { ticks.minOf { it.amount } .. ticks.maxOf { it.amount } }
+            }
+            val graphDomain by remember {
+                derivedStateOf { TimeDomain(ticks.minOf { it.timeForData } .. ticks.maxOf { it.timeForData }) }
+            }
+            val graphDomainLimits by remember {
+                derivedStateOf { TimeDomain(Instant.DISTANT_PAST, Instant.DISTANT_FUTURE, true) }
+            }
+
+            CounterHistoryScreen(
+                counter = counter,
+                ticks = ticks,
+                tickSum = tickSum,
+                tickAverage = tickAverage,
+                graphRange = graphRange,
+                graphDomain = graphDomain,
+                graphDomainLimits = graphDomainLimits,
+                graphDomainOptions = listOf(),
+                changeGraphDomain = {},
+                onAddTick = {},
+                onDeleteTick = {},
+                onResetCounter = {},
+                onCounterChange = {},
+                onNavigateUp = {},
+            )
+        }
     }
 
     @Test
--- app/src/androidTest/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOverviewScreenTest.kt
+++ app/src/androidTest/java/org/calamarfederal/messyink/feature_counter/presentation/counter_overview/CounterOverviewScreenTest.kt
@@ -1,52 +1,71 @@
 package org.calamarfederal.messyink.feature_counter.presentation.counter_overview
 
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.semantics.SemanticsActions
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performSemanticsAction
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.runBlocking
 import org.calamarfederal.messyink.MainActivity
 import org.calamarfederal.messyink.OnCreateHookImpl
 import org.calamarfederal.messyink.data.CounterTickDao
+import org.calamarfederal.messyink.feature_counter.data.generateCounters
 import org.calamarfederal.messyink.feature_counter.data.generateTestData
+import org.calamarfederal.messyink.feature_counter.data.toCounter
+import org.calamarfederal.messyink.feature_counter.domain.use_case.toUI
+import org.calamarfederal.messyink.feature_counter.presentation.state.UiCounter
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import javax.inject.Inject
 
 /**
  * # Counter Overview Screen
  * ## Unit Tests
  */
-@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
 class CounterOverviewScreenTest {
+    @get:Rule
+    val composeRule = createComposeRule()
 
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
-
-    @get:Rule(order = 1)
-    val composeRule = createAndroidComposeRule<MainActivity>()
-
-    private val testContent = CounterOverviewContent()
-
-    @BindValue
-    val contentSetter: OnCreateHookImpl = testContent
-
-    @Inject
-    lateinit var dao: CounterTickDao
+    lateinit var countersState: MutableStateFlow<List<UiCounter>>
+    lateinit var tickSumState: MutableStateFlow<Map<Long, Double>>
 
     @Before
     fun setUp() {
-        hiltRule.inject()
+        countersState =
+            MutableStateFlow(generateCounters().take(5).map { it.toCounter().toUI() }.toList())
+        tickSumState = MutableStateFlow(countersState.value.associate { it.id to 1.00 })
 
-        runBlocking {
-            dao.generateTestData()
+        composeRule.setContent {
+            val counters by countersState.collectAsState()
+            val tickSum by tickSumState.collectAsState()
+
+            CounterOverviewScreen(
+                counters = counters,
+                tickSums = tickSum,
+                onCounterIncrement = {},
+                onCounterDecrement = {},
+                onDeleteCounter = {},
+                onClearCounterTicks = {},
+                onCreateCounter = {},
+                onNavigateToCounterDetails = {},
+                onNavigateToCounterGameMode = {},
+                onNavigateToCounterEdit = {},
+            )
         }
     }
 
